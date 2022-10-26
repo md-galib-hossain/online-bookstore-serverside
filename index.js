58cffd85e1c39cb666fed8bdd4a1d81ec606e628
Md Galib Hossain <mdgalib23@gmail.com>
2022-10-26 14:00:00 +0600

    fixing git error

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORt || 5000;
+
+app.use(cors());
+const categories = require("./data/categories.json");
+const courses = require("./data/courses.json");
+app.get("/", (req, res) => {
+  res.send("course api running");
+});
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
+app.get("/courses/:id", (req, res) => {
+  console.log(req.params.id);
+});
+app.listen(port, () => {
+  console.log("server running on port:", port);
+});
